e68beee9e8544feda525ef16d12d168a3f36b840
Bless Ambele <blessambel1@gmail.com>
2022-03-11 12:00:35 -0930

    enabling google oauth part-1

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
+require("dotenv");
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
-passport.use(new GoogleStrategy());
+console.log(process.env.GOOGLE_CLIENT_SECRET);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost/callback",
+    },
+    function () {}
+  )
+);
 
 app.get("/", (req, res) => {
   res.send({ hi: "hello" });
