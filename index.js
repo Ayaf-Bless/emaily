0d8a68ab4f3b9f3e38bd53c3fd9b431449390d1c
Bless Ambele <blessambel1@gmail.com>
2022-03-11 11:16:29 -0930

    setting up google auth strategy

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
+passport.use(new GoogleStrategy());
 
 app.get("/", (req, res) => {
   res.send({ hi: "hello" });
 });
 
-app.listen(3000, () => {
-  console.log("app is running");
+app.listen(5000, () => {
+  console.log(`app is running 5000`);
 });
