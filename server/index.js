da7ee16b973bbaa22803651a3bd2b268cbc82594
Bless Ambele <blessambel1@gmail.com>
2022-03-14 20:56:42 -0930

    setting mongo db

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const dotEnv = require("dotenv");
 const express = require("express");
 dotEnv.config({ path: "./config.env" });
 const authRouter = require("./routes/authRoute");
+const mongoose = require("mongoose");
 require("./services/passport");
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/emaily")
+  .then((r) => console.log("DB connected"));
 const app = express();
-
 app.use(authRouter);
 
 app.listen(5000, () => {
