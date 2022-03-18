eef9ab0deb00a04a1bc19a0c09df5be64a46774a
Bless Ambele <blessambel1@gmail.com>
2022-03-17 23:29:35 -0930

    setting the cookie-session to be used as the auth-middleware

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const dotEnv = require("dotenv");
-const express = require("express");
 dotEnv.config({ path: "./config.env" });
+const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const authRouter = require("./routes/authRoute");
 const mongoose = require("mongoose");
 require("./services/passport");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/emaily")
-  .then((r) => console.log("DB connected"));
+  .then(() => console.log("DB connected"));
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(authRouter);
 
 app.listen(5000, () => {
