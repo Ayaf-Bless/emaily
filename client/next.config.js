8f45a3d795a3dc10881bd0d4657c544d7f727042
Bless Ambele <blessambel1@gmail.com>
2022-03-22 20:59:30 -0930

    add a proxy to the next js app to make calling api easy

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/auth/google",
+        destination: "http://localhost:5000/auth/google", // Proxy to Backend
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
