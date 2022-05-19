ddee01abbbdcf7359f0d8f9c295c3ce7b46ce5c3
Ayaf-Bless <blessambel1@gmail.com>
2022-05-19 12:13:26 -0900

    initialize the component

---
+++ client/components/SurveyNew.js
@@ -0,0 +1,9 @@
+const SurveyNew = () => {
+  return (
+    <div>
+      <h1>Survey</h1>
+    </div>
+  );
+};
+
+export default SurveyNew;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <Link href={"/auth/google"}>Sign up with google</Link>
     </div>
   );
